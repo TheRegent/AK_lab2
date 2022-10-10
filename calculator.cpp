85593e0e72e0f72a8b5ab87eb95e3fe361fcb988
TheRegent <kobilinskiy.fedir@gmail.com>
2022-10-10 23:37:04 +0300

    Commits adding

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,28 @@
 #include "calculator.h"
 
+//function that add doubles
 int Calculator::Add (double a, double b)
 {
+	//function that  return adding doubles
+
 	return a + b + 0.5;
 }
 
+//function that substracts doubles
+
 int Calculator::Sub (double a, double b)
 {
+	//function that subsrtacts doubles
+
     return Add (a, -b);
 }
 
+//function that multimply doubles
+
 int Calculator::Mul (double a, double b)
 {
+	//function that multyplying doubles
+
     return a * b + 0.5;
 }
+//=)
